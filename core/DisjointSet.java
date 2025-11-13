355d8c7bed5ca3a0ec3a65a26757b0eed5729b71
Adilzhan Zhumash <zhumash.adilzhan@gmail.com>
2025-11-13 20:19:31 +0500

    Add DisjointSet for union-find operations used in Kruskal

---
+++ core/DisjointSet.java
@@ -0,0 +1,37 @@
+package com.example.mst.core;
+
+public class DisjointSet {
+    private final int[] parent;
+    private final int[] rank;
+
+    public DisjointSet(int V) {
+        parent = new int[V];
+        rank = new int[V];
+        for (int i = 0; i < V; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int i) {
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
+        }
+        return parent[i];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
